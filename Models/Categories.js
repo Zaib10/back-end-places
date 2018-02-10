3f6a8659f5d784040b304b5c344ec2ea5edb3d46
shah zaib <shahzaib.shebi@gmail.com>
2018-02-10 20:55:58 +0500

    models modified

--- Models/Categories.js
+++ Models/Categories.js
@@ -6,7 +6,7 @@ const Categories = Mongoose.Schema({
     title: { type: String, required: true, trim: true, unique: true }, // Art and Fashion
    // slug: { type: String, required: true, trim: true, unique: true }, // art-and-fashion
     details: { type: String, trim: true },
-    places: { type: Mongoose.Schema.Types.ObjectId, ref: 'places' },
+    //places: { type: Mongoose.Schema.Types.ObjectId, ref: 'places' },
 })
 const category = Mongoose.model('category', Categories, 'category');
 
